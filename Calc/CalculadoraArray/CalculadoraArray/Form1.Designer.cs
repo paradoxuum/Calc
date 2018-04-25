bc13ecff3e92fff0f49dce3e006cd7f6447d904f
Eduardo <haevorak@gmail.com>
2018-04-24 22:38:06 -0300

    Calculadora Array

---
+++ Calc/CalculadoraArray/CalculadoraArray/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace CalculadoraArray
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View));
+            this.btnSete = new System.Windows.Forms.Button();
+            this.btnOito = new System.Windows.Forms.Button();
+            this.btnNove = new System.Windows.Forms.Button();
+            this.btnSeis = new System.Windows.Forms.Button();
+            this.btnCinco = new System.Windows.Forms.Button();
+            this.btnQuatro = new System.Windows.Forms.Button();
+            this.btnTres = new System.Windows.Forms.Button();
+            this.btnDois = new System.Windows.Forms.Button();
+            this.btnUm = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubt = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDivi = new System.Windows.Forms.Button();
+            this.txtBoxVisor = new System.Windows.Forms.TextBox();
+            this.lblTituloVisor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSete
+            // 
+            this.btnSete.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSete.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSete, "btnSete");
+            this.btnSete.Name = "btnSete";
+            this.btnSete.UseVisualStyleBackColor = true;
+            this.btnSete.Click += new System.EventHandler(this.btnSete_Click);
+            // 
+            // btnOito
+            // 
+            this.btnOito.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnOito.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnOito, "btnOito");
+            this.btnOito.Name = "btnOito";
+            this.btnOito.UseVisualStyleBackColor = true;
+            // 
+            // btnNove
+            // 
+            this.btnNove.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnNove.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnNove, "btnNove");
+            this.btnNove.Name = "btnNove";
+            this.btnNove.UseVisualStyleBackColor = true;
+            // 
+            // btnSeis
+            // 
+            this.btnSeis.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSeis.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSeis, "btnSeis");
+            this.btnSeis.Name = "btnSeis";
+            this.btnSeis.UseVisualStyleBackColor = true;
+            // 
+            // btnCinco
+            // 
+            this.btnCinco.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCinco.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnCinco, "btnCinco");
+            this.btnCinco.Name = "btnCinco";
+            this.btnCinco.UseVisualStyleBackColor = true;
+            // 
+            // btnQuatro
+            // 
+            this.btnQuatro.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnQuatro.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnQuatro, "btnQuatro");
+            this.btnQuatro.Name = "btnQuatro";
+            this.btnQuatro.UseVisualStyleBackColor = true;
+            // 
+            // btnTres
+            // 
+            this.btnTres.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnTres.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnTres, "btnTres");
+            this.btnTres.Name = "btnTres";
+            this.btnTres.UseVisualStyleBackColor = true;
+            // 
+            // btnDois
+            // 
+            this.btnDois.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDois.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnDois, "btnDois");
+            this.btnDois.Name = "btnDois";
+            this.btnDois.UseVisualStyleBackColor = true;
+            // 
+            // btnUm
+            // 
+            this.btnUm.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnUm.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnUm, "btnUm");
+            this.btnUm.Name = "btnUm";
+            this.btnUm.UseVisualStyleBackColor = true;
+            // 
+            // btnZero
+            // 
+            this.btnZero.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnZero.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnZero, "btnZero");
+            this.btnZero.Name = "btnZero";
+            this.btnZero.UseVisualStyleBackColor = true;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnIgual.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnIgual, "btnIgual");
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSoma.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSoma, "btnSoma");
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            // 
+            // btnSubt
+            // 
+            this.btnSubt.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSubt.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSubt, "btnSubt");
+            this.btnSubt.Name = "btnSubt";
+            this.btnSubt.UseVisualStyleBackColor = true;
+            // 
+            // btnMult
+            // 
+            this.btnMult.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMult.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnMult, "btnMult");
+            this.btnMult.Name = "btnMult";
+            this.btnMult.UseVisualStyleBackColor = true;
+            // 
+            // btnDivi
+            // 
+            this.btnDivi.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDivi.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnDivi, "btnDivi");
+            this.btnDivi.Name = "btnDivi";
+            this.btnDivi.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxVisor
+            // 
+            this.txtBoxVisor.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.txtBoxVisor, "txtBoxVisor");
+            this.txtBoxVisor.Name = "txtBoxVisor";
+            // 
+            // lblTituloVisor
+            // 
+            resources.ApplyResources(this.lblTituloVisor, "lblTituloVisor");
+            this.lblTituloVisor.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblTituloVisor.Name = "lblTituloVisor";
+            // 
+            // View
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.lblTituloVisor);
+            this.Controls.Add(this.txtBoxVisor);
+            this.Controls.Add(this.btnDivi);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSubt);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnTres);
+            this.Controls.Add(this.btnDois);
+            this.Controls.Add(this.btnUm);
+            this.Controls.Add(this.btnSeis);
+            this.Controls.Add(this.btnCinco);
+            this.Controls.Add(this.btnQuatro);
+            this.Controls.Add(this.btnNove);
+            this.Controls.Add(this.btnOito);
+            this.Controls.Add(this.btnSete);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "View";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSete;
+        private System.Windows.Forms.Button btnOito;
+        private System.Windows.Forms.Button btnNove;
+        private System.Windows.Forms.Button btnSeis;
+        private System.Windows.Forms.Button btnCinco;
+        private System.Windows.Forms.Button btnQuatro;
+        private System.Windows.Forms.Button btnTres;
+        private System.Windows.Forms.Button btnDois;
+        private System.Windows.Forms.Button btnUm;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubt;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDivi;
+        private System.Windows.Forms.TextBox txtBoxVisor;
+        private System.Windows.Forms.Label lblTituloVisor;
+    }
+}
+
---
+++ Calc/CalculadoraArray/CalculadoraArray/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculadoraArray
+{
+    public partial class View : Form
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSete_Click(object sender, EventArgs e)
+        {
+            txtBoxVisor.Text = System.Convert.ToDouble();
+            if (txtBoxVisor.Text =) 
+            {
+
+            }
+        }
+    }
+}
