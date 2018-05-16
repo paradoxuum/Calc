e39aab88eedb9495801b156b3f61cbe0c7c32649
Eduardo <haevorak@gmail.com>
2018-05-15 22:40:49 -0300

    MB

---
+++ CalculadoraArray/CalculadoraArray/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace CalculadoraArray
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(View));
+            this.btnSete = new System.Windows.Forms.Button();
+            this.btnOito = new System.Windows.Forms.Button();
+            this.btnNove = new System.Windows.Forms.Button();
+            this.btnSeis = new System.Windows.Forms.Button();
+            this.btnCinco = new System.Windows.Forms.Button();
+            this.btnQuatro = new System.Windows.Forms.Button();
+            this.btnTres = new System.Windows.Forms.Button();
+            this.btnDois = new System.Windows.Forms.Button();
+            this.btnUm = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubt = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDivi = new System.Windows.Forms.Button();
+            this.txtlblResultado = new System.Windows.Forms.TextBox();
+            this.btnC = new System.Windows.Forms.Button();
+            this.txtBoxVisor = new System.Windows.Forms.Label();
+            this.btnPFim = new System.Windows.Forms.Button();
+            this.btnPInit = new System.Windows.Forms.Button();
+            this.btnPonto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSete
+            // 
+            this.btnSete.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSete.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSete, "btnSete");
+            this.btnSete.Name = "btnSete";
+            this.btnSete.UseVisualStyleBackColor = true;
+            this.btnSete.Click += new System.EventHandler(this.btnSete_Click);
+            // 
+            // btnOito
+            // 
+            this.btnOito.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnOito.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnOito, "btnOito");
+            this.btnOito.Name = "btnOito";
+            this.btnOito.UseVisualStyleBackColor = true;
+            this.btnOito.Click += new System.EventHandler(this.btnOito_Click);
+            // 
+            // btnNove
+            // 
+            this.btnNove.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnNove.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnNove, "btnNove");
+            this.btnNove.Name = "btnNove";
+            this.btnNove.UseVisualStyleBackColor = true;
+            this.btnNove.Click += new System.EventHandler(this.btnNove_Click);
+            // 
+            // btnSeis
+            // 
+            this.btnSeis.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSeis.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSeis, "btnSeis");
+            this.btnSeis.Name = "btnSeis";
+            this.btnSeis.UseVisualStyleBackColor = true;
+            this.btnSeis.Click += new System.EventHandler(this.btnSeis_Click);
+            // 
+            // btnCinco
+            // 
+            this.btnCinco.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCinco.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnCinco, "btnCinco");
+            this.btnCinco.Name = "btnCinco";
+            this.btnCinco.UseVisualStyleBackColor = true;
+            this.btnCinco.Click += new System.EventHandler(this.btnCinco_Click);
+            // 
+            // btnQuatro
+            // 
+            this.btnQuatro.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnQuatro.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnQuatro, "btnQuatro");
+            this.btnQuatro.Name = "btnQuatro";
+            this.btnQuatro.UseVisualStyleBackColor = true;
+            this.btnQuatro.Click += new System.EventHandler(this.btnQuatro_Click);
+            // 
+            // btnTres
+            // 
+            this.btnTres.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnTres.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnTres, "btnTres");
+            this.btnTres.Name = "btnTres";
+            this.btnTres.UseVisualStyleBackColor = true;
+            this.btnTres.Click += new System.EventHandler(this.btnTres_Click);
+            // 
+            // btnDois
+            // 
+            this.btnDois.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDois.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnDois, "btnDois");
+            this.btnDois.Name = "btnDois";
+            this.btnDois.UseVisualStyleBackColor = true;
+            this.btnDois.Click += new System.EventHandler(this.btnDois_Click);
+            // 
+            // btnUm
+            // 
+            this.btnUm.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnUm.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnUm, "btnUm");
+            this.btnUm.Name = "btnUm";
+            this.btnUm.UseVisualStyleBackColor = true;
+            this.btnUm.Click += new System.EventHandler(this.btnUm_Click);
+            // 
+            // btnZero
+            // 
+            this.btnZero.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnZero.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnZero, "btnZero");
+            this.btnZero.Name = "btnZero";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnIgual.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnIgual, "btnIgual");
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSoma.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSoma, "btnSoma");
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSubt
+            // 
+            this.btnSubt.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSubt.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnSubt, "btnSubt");
+            this.btnSubt.Name = "btnSubt";
+            this.btnSubt.UseVisualStyleBackColor = true;
+            this.btnSubt.Click += new System.EventHandler(this.btnSubt_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMult.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnMult, "btnMult");
+            this.btnMult.Name = "btnMult";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDivi
+            // 
+            this.btnDivi.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDivi.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnDivi, "btnDivi");
+            this.btnDivi.Name = "btnDivi";
+            this.btnDivi.UseVisualStyleBackColor = true;
+            this.btnDivi.Click += new System.EventHandler(this.btnDivi_Click);
+            // 
+            // txtlblResultado
+            // 
+            this.txtlblResultado.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.txtlblResultado, "txtlblResultado");
+            this.txtlblResultado.Name = "txtlblResultado";
+            // 
+            // btnC
+            // 
+            this.btnC.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnC.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnC, "btnC");
+            this.btnC.Name = "btnC";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // txtBoxVisor
+            // 
+            resources.ApplyResources(this.txtBoxVisor, "txtBoxVisor");
+            this.txtBoxVisor.Name = "txtBoxVisor";
+            // 
+            // btnPFim
+            // 
+            this.btnPFim.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnPFim.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnPFim, "btnPFim");
+            this.btnPFim.Name = "btnPFim";
+            this.btnPFim.UseVisualStyleBackColor = true;
+            this.btnPFim.Click += new System.EventHandler(this.btnPfim_Click);
+            // 
+            // btnPInit
+            // 
+            this.btnPInit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnPInit.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnPInit, "btnPInit");
+            this.btnPInit.Name = "btnPInit";
+            this.btnPInit.UseVisualStyleBackColor = true;
+            this.btnPInit.Click += new System.EventHandler(this.btnPInit_Click);
+            // 
+            // btnPonto
+            // 
+            this.btnPonto.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnPonto.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnPonto, "btnPonto");
+            this.btnPonto.Name = "btnPonto";
+            this.btnPonto.UseVisualStyleBackColor = true;
+            this.btnPonto.Click += new System.EventHandler(this.btnPonto_Click);
+            // 
+            // View
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.btnPonto);
+            this.Controls.Add(this.btnPInit);
+            this.Controls.Add(this.btnPFim);
+            this.Controls.Add(this.txtBoxVisor);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.txtlblResultado);
+            this.Controls.Add(this.btnDivi);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnSubt);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnTres);
+            this.Controls.Add(this.btnDois);
+            this.Controls.Add(this.btnUm);
+            this.Controls.Add(this.btnSeis);
+            this.Controls.Add(this.btnCinco);
+            this.Controls.Add(this.btnQuatro);
+            this.Controls.Add(this.btnNove);
+            this.Controls.Add(this.btnOito);
+            this.Controls.Add(this.btnSete);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "View";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSete;
+        private System.Windows.Forms.Button btnOito;
+        private System.Windows.Forms.Button btnNove;
+        private System.Windows.Forms.Button btnSeis;
+        private System.Windows.Forms.Button btnCinco;
+        private System.Windows.Forms.Button btnQuatro;
+        private System.Windows.Forms.Button btnTres;
+        private System.Windows.Forms.Button btnDois;
+        private System.Windows.Forms.Button btnUm;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubt;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDivi;
+        private System.Windows.Forms.TextBox txtlblResultado;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Label txtBoxVisor;
+        private System.Windows.Forms.Button btnPFim;
+        private System.Windows.Forms.Button btnPInit;
+        private System.Windows.Forms.Button btnPonto;
+    }
+}
+
---
+++ CalculadoraArray/CalculadoraArray/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler; //fornece classes para representar a estrutura lógica do código-fonte (possibilita anexar outras linguagem).
+using System.Reflection;
+
+namespace CalculadoraArray
+{ 
+    public partial class View : Form
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+
+        private double Calculador(string expressao)
+        {
+            //fornece acesso a instâncias do gerador de código C# e do compilador de código.
+            CSharpCodeProvider moduloProvedor = new CSharpCodeProvider();
+
+            // Cria os parmaetros para a compilação origem
+            CompilerParameters moduloCompilador = new CompilerParameters();
+                                                                                   //********************Calcula a expressão***********************
+            string Resolver = "namespace ns{" + "using System;" + "class class1{" + "public static double Evaluate(){return " + expressao + ";}}} ";  
+            
+
+            //representa o resultado da compilação que é retornado a partir do compilador.
+            CompilerResults cr = moduloProvedor.CompileAssemblyFromSource(moduloCompilador, Resolver);
+
+            //retorna mensagem de erro caso a expressão seja invalida.
+            if (cr.Errors.Count > 0)
+            {
+                throw new ArgumentException("Inválida");
+            }
+            else
+            {
+                MethodInfo Methinfo = cr.CompiledAssembly.GetType("ns.class1").GetMethod("Evaluate");
+                return (double)Methinfo.Invoke(null, null);
+            }
+        }
+
+        private void btnNove_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "9";
+        }
+
+        private void btnOito_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "8";
+        }
+
+        private void btnSete_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "7";
+        }
+
+        private void btnSeis_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "6";
+        }
+
+        private void btnCinco_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "5";
+        }
+
+        private void btnQuatro_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "4";
+        }
+
+        private void btnTres_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "3";
+        }
+
+        private void btnDois_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "2";
+        }
+
+        private void btnUm_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "1";
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "0";
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "+";
+        }
+
+        private void btnSubt_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "-";
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "*";
+        }
+
+        private void btnDivi_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "/";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtBoxVisor.Text = txtlblResultado.Text + " = " + Calculador(txtlblResultado.Text).ToString();
+                txtlblResultado.Text = "Resultado";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Expressão " + ex.Message.ToString());
+            }
+        }
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = "";
+            txtBoxVisor.Text = "Resultado";
+        }
+
+        private void btnPfim_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + ")";
+        }
+
+        private void btnPInit_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + "(";
+        }
+
+        private void btnPonto_Click(object sender, EventArgs e)
+        {
+            txtlblResultado.Text = txtlblResultado.Text + ".";
+        }
+    }
+}
